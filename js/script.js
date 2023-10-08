e4d4dfd2fd9bc48bd89f9aa2ec8fed073148e88e
jorge-marcelo <jorge3258@hotmail.com>
2023-10-07 21:17:01 -0300

    Primeiro Commit

---
+++ js/script.js
@@ -0,0 +1,58 @@
+/*
+Autor: Jorge Marcelo
+Data de Criação: 07/10/2023
+Github: https://github.com/Jorge-Marcelo
+Linkedin: https://www.linkedin.com/in/jorge-marcelo-067a5017b/
+Instagram: https://www.instagram.com/10_stronger/
+Threads: https://www.threads.net/@10_stronger
+*/
+
+/*Cominucação do HTML com JS usando os botões e input*/
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+/*Adiciona um novo item na lista ao apertar o botão enter do teclado
+ou ao clicar caso contrario não adiciona nada por isso o null*/
+button.addEventListener("click", adicionarLista);
+input.addEventListener("keyup", (evento) => {
+evento.keyCode === 13 ? adicionarLista(evento) : null;
+});
+
+/*Fzendo a comunicação do DOM pegando 
+todos os itens da lista e botões*/
+function adicionarLista(evento) {
+const incompleta = document.querySelector(".incompleta");
+const completa = document.querySelector(".completa");
+const novoItemLista = document.createElement("li");
+const marcarButton = document.createElement("button");
+const deletarButton = document.createElement("button");
+
+/*Adicionar botões de completo e deletar ao HTML*/
+marcarButton.innerHTML = '<i class="fa fa-check"></i>';
+deletarButton.innerHTML = '<i class="fa fa-trash"></i>';
+
+/*Se o input de digitar tarefa não estiver vazio
+vai adicionar todos os itens da lista*/
+if (input.value !== "") {
+novoItemLista.textContent = input.value;
+input.value = "";
+incompleta.appendChild(novoItemLista);
+novoItemLista.appendChild(marcarButton);
+novoItemLista.appendChild(deletarButton);
+}
+
+/*Escutador de evento caso a tarefa estiver marcada como concluida
+vai adicionar as atrefas concluidas e vai remover das atefas incompletas*/
+marcarButton.addEventListener("click", function () {
+const itemLista = this.parentNode;
+itemLista.remove();
+completa.appendChild(itemLista);
+marcarButton.style.display = "none";
+});
+
+/*Mesa função de cima mas vai deletar a tarefa seja ela completa ou não*/
+deletarButton.addEventListener("click", function () {
+const itemLista = this.parentNode;
+itemLista.remove();
+});
+}
